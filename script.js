c915618c1d899c0b1f26a68e27b433c01d5c0563
Pankaj1105 <98206434+Pankaj1105@users.noreply.github.com>
2023-04-24 21:03:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 //your JS code here. If required.
-//your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function() {
-  // Get a reference to the body tag
-  const body = document.querySelector("body");
-
-  // Create a new text node with the message
-  const message = document.createTextNode("DOM load success");
-
-  // Append the text node to the body tag
-  body.appendChild(message);
+      let id = document.body.children[0];
+      id.innerText = 'DOM load success';
+  
+});
